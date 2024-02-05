459faba74c39a8064133bdbabf1f0aecf4ebf207
Rghhemendra Gour <hemendramu76@gmail.com>
2024-02-05 20:29:21 +0530

    util code discussion

--- scr/index.js
+++ scr/index.js
@@ -3,12 +3,20 @@ dotenv.config();
 import connectDB from "./db/index.js";
 
 
-// dotenv.config({
-//     path: './env'
-// })
+dotenv.config({
+    path: './env'
+})
 
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Sertver is runing at port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("monogo db connection !!!", err)
+    })
 
 
 
